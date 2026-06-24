f4f08a192b10786d7feee2dc5d29671525d1b9e8
Sylvester Junior Ampomah <91274489+Sylvester107@users.noreply.github.com>
2026-06-23 23:29:44 -0400

    test twilio voice call successful

---
+++ tests/twilio_test.py
@@ -0,0 +1,17 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+    url="https://webhooks.twilio.com/v1/Voice/Template/voice_auto_response",
+    to="+15719778953",
+    from_="+18338593215",
+)
+
+print(call.sid)
